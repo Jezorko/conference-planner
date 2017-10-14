8b1bb460503df274382ce00b55f8bc712bb06379
Paweł Fiuk <pawelfiu@gmail.com>
2017-10-14 22:54:38 +0200

    First working implementation of TalkData deserializer

--- src/main/java/io/TalkDataFromStringDeserializer.java
+++ src/main/java/io/TalkDataFromStringDeserializer.java
@@ -1,9 +1,35 @@
 package io;
 
 import core.TalkData;
+import org.apache.commons.validator.routines.RegexValidator;
+
+import static io.vavr.control.Option.of;
+import static java.lang.Integer.valueOf;
+import static org.apache.commons.lang3.StringUtils.split;
 
 public class TalkDataFromStringDeserializer {
+
+    private static final String TALK_DATA_SPECIFICATION_PATTERN = "^\\D+ ((\\d+min)|(lightning))$";
+
+    private final RegexValidator talkDataValidator = new RegexValidator(TALK_DATA_SPECIFICATION_PATTERN);
+
     public TalkData deserialize(String line) {
-        return null;
+        return of(line).filter(talkDataValidator::isValid)
+                       .map(this::extractFromValidString)
+                       .getOrElseThrow(IllegalArgumentException::new);
+    }
+
+    private TalkData extractFromValidString(String line) {
+        String[] lineParts = split(line);
+        StringBuilder name = new StringBuilder();
+        for (int i = 0; i < lineParts.length - 1; i++) {
+            name.append(lineParts[i]);
+            if (i < lineParts.length - 2) {
+                name.append(" ");
+            }
+        }
+        final String timePart = lineParts[lineParts.length - 1];
+        int timeInMinutes = ("lightning".equals(timePart) ? 5 : valueOf(timePart.substring(0, timePart.indexOf("min"))));
+        return new TalkData(name.toString(), timeInMinutes);
     }
 }
