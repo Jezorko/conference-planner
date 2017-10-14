c082c5d6371eed18445a064004c83c01acd8fcca
Paweł Fiuk <pawelfiu@gmail.com>
2017-10-14 22:55:19 +0200

    Refactoring of TalkData deserializer

--- src/main/java/io/TalkDataFromStringDeserializer.java
+++ src/main/java/io/TalkDataFromStringDeserializer.java
@@ -5,7 +5,6 @@ import org.apache.commons.validator.routines.RegexValidator;
 
 import static io.vavr.control.Option.of;
 import static java.lang.Integer.valueOf;
-import static org.apache.commons.lang3.StringUtils.split;
 
 public class TalkDataFromStringDeserializer {
 
@@ -20,16 +19,7 @@ public class TalkDataFromStringDeserializer {
     }
 
     private TalkData extractFromValidString(String line) {
-        String[] lineParts = split(line);
-        StringBuilder name = new StringBuilder();
-        for (int i = 0; i < lineParts.length - 1; i++) {
-            name.append(lineParts[i]);
-            if (i < lineParts.length - 2) {
-                name.append(" ");
-            }
-        }
-        final String timePart = lineParts[lineParts.length - 1];
-        int timeInMinutes = ("lightning".equals(timePart) ? 5 : valueOf(timePart.substring(0, timePart.indexOf("min"))));
-        return new TalkData(name.toString(), timeInMinutes);
+        String name = line.replaceAll(" ((\\d+min)|(lightning))", "");
+        return new TalkData(name, line.endsWith("lightning") ? 5 : valueOf(line.replaceAll("\\D", "")));
     }
 }
