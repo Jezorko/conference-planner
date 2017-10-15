48055d77a41477a67c04235bacce6a66a2e22183
Paweł Fiuk <pawelfiu@gmail.com>
2017-10-15 22:23:53 +0200

    Added line separator property to TrackDataSerializer test
    Without it the test wouldn't pass on Windows

--- src/test/groovy/io/TrackDataToStringSerializerSpecTest.groovy
+++ src/test/groovy/io/TrackDataToStringSerializerSpecTest.groovy
@@ -5,8 +5,11 @@ import core.TrackData
 import spock.lang.Specification
 import spock.lang.Unroll
 
+import static System.getProperty
+
 class TrackDataToStringSerializerSpecTest extends Specification {
 
+    static LINE_SEPARATOR = getProperty("line.separator")
     def serializer = new TrackDataToStringSerializer()
 
     def "should serialize input as in specification"() {
@@ -29,18 +32,18 @@ class TrackDataToStringSerializerSpecTest extends Specification {
                        new TalkData("Ruby vs. Clojure for Back-End Development", 30),
                        new TalkData("User Interface CSS in Rails Apps", 30)]) |
                 1                                                                          ||
-                'Track 1\n' +
-                '09:00AM Writing Fast Tests Against Enterprise Rails 60min\n' +
-                '10:00AM Overdoing it in Python 45min\n' +
-                '10:45AM Lua for the Masses 30min\n' +
-                '11:15AM Ruby Errors from Mismatched Gem Versions 45min\n' +
-                '12:00PM Lunch\n' +
-                '01:00PM Ruby on Rails: Why We Should Move On 60min\n' +
-                '02:00PM Common Ruby Errors 45min\n' +
-                '02:45PM Pair Programming vs Noise 45min\n' +
-                '03:30PM Programming in the Boondocks of Seattle 30min\n' +
-                '04:00PM Ruby vs. Clojure for Back-End Development 30min\n' +
-                '04:30PM User Interface CSS in Rails Apps 30min\n' +
+                'Track 1' + LINE_SEPARATOR +
+                '09:00AM Writing Fast Tests Against Enterprise Rails 60min' + LINE_SEPARATOR +
+                '10:00AM Overdoing it in Python 45min' + LINE_SEPARATOR +
+                '10:45AM Lua for the Masses 30min' + LINE_SEPARATOR +
+                '11:15AM Ruby Errors from Mismatched Gem Versions 45min' + LINE_SEPARATOR +
+                '12:00PM Lunch' + LINE_SEPARATOR +
+                '01:00PM Ruby on Rails: Why We Should Move On 60min' + LINE_SEPARATOR +
+                '02:00PM Common Ruby Errors 45min' + LINE_SEPARATOR +
+                '02:45PM Pair Programming vs Noise 45min' + LINE_SEPARATOR +
+                '03:30PM Programming in the Boondocks of Seattle 30min' + LINE_SEPARATOR +
+                '04:00PM Ruby vs. Clojure for Back-End Development 30min' + LINE_SEPARATOR +
+                '04:30PM User Interface CSS in Rails Apps 30min' + LINE_SEPARATOR +
                 '05:00PM Networking Event'
 
         new TrackData([new TalkData("Communicating Over Distance", 60),
@@ -53,17 +56,17 @@ class TrackDataToStringSerializerSpecTest extends Specification {
                        new TalkData("Ruby on Rails Legacy App Maintenance", 60),
                        new TalkData("Rails for Python Developers", 5)])       |
                 2                                                                          ||
-                'Track 2\n' +
-                '09:00AM Communicating Over Distance 60min\n' +
-                '10:00AM Rails Magic 60min\n' +
-                '11:00AM Woah 30min\n' +
-                '11:30AM Sit Down and Write 30min\n' +
-                '12:00PM Lunch\n' +
-                '01:00PM Accounting-Driven Development 45min\n' +
-                '01:45PM Clojure Ate Scala (on my project) 45min\n' +
-                '02:30PM A World Without HackerNews 30min\n' +
-                '03:00PM Ruby on Rails Legacy App Maintenance 60min\n' +
-                '04:00PM Rails for Python Developers lightning\n' +
+                'Track 2' + LINE_SEPARATOR +
+                '09:00AM Communicating Over Distance 60min' + LINE_SEPARATOR +
+                '10:00AM Rails Magic 60min' + LINE_SEPARATOR +
+                '11:00AM Woah 30min' + LINE_SEPARATOR +
+                '11:30AM Sit Down and Write 30min' + LINE_SEPARATOR +
+                '12:00PM Lunch' + LINE_SEPARATOR +
+                '01:00PM Accounting-Driven Development 45min' + LINE_SEPARATOR +
+                '01:45PM Clojure Ate Scala (on my project) 45min' + LINE_SEPARATOR +
+                '02:30PM A World Without HackerNews 30min' + LINE_SEPARATOR +
+                '03:00PM Ruby on Rails Legacy App Maintenance 60min' + LINE_SEPARATOR +
+                '04:00PM Rails for Python Developers lightning' + LINE_SEPARATOR +
                 '04:05PM Networking Event'
     }
 
