a15f461273ce42a85752f27b0d03b305bf006247
Paweł Fiuk <pawelfiu@gmail.com>
2017-10-15 21:06:08 +0200

    Added test case with the first track data from the specification

--- src/test/groovy/io/TrackDataToStringSerializerSpecTest.groovy
+++ src/test/groovy/io/TrackDataToStringSerializerSpecTest.groovy
@@ -10,19 +10,50 @@ class TrackDataToStringSerializerSpecTest extends Specification {
     def serializer = new TrackDataToStringSerializer()
 
     def "should serialize input as in specification"() {
-        given:
-        def input = new TrackData([new TalkData("Communicating Over Distance", 60),
-                                   new TalkData("Rails Magic", 60),
-                                   new TalkData("Woah", 30),
-                                   new TalkData("Sit Down and Write", 30)],
-                                  [new TalkData("Accounting-Driven Development", 45),
-                                   new TalkData("Clojure Ate Scala (on my project)", 45),
-                                   new TalkData("A World Without HackerNews", 30),
-                                   new TalkData("Ruby on Rails Legacy App Maintenance", 60),
-                                   new TalkData("Rails for Python Developers", 5)])
+        when:
+        def output = serializer.serialize(input, trackIndex)
 
-        and:
-        def expectedOutput = 'Track 1\n' +
+        then:
+        expectedOutput == output
+
+        where:
+        input                                                                 | trackIndex || expectedOutput
+        new TrackData([new TalkData("Writing Fast Tests Against Enterprise Rails", 60),
+                       new TalkData("Overdoing it in Python", 45),
+                       new TalkData("Lua for the Masses", 30),
+                       new TalkData("Ruby Errors from Mismatched Gem Versions", 45)],
+                      [new TalkData("Ruby on Rails: Why We Should Move On", 60),
+                       new TalkData("Common Ruby Errors", 45),
+                       new TalkData("Pair Programming vs Noise", 45),
+                       new TalkData("Programming in the Boondocks of Seattle", 30),
+                       new TalkData("Ruby vs. Clojure for Back-End Development", 30),
+                       new TalkData("User Interface CSS in Rails Apps", 30)]) |
+                1                                                                          ||
+                'Track 1\n' +
+                '09:00AM Writing Fast Tests Against Enterprise Rails 60min\n' +
+                '10:00AM Overdoing it in Python 45min\n' +
+                '10:45AM Lua for the Masses 30min\n' +
+                '11:15AM Ruby Errors from Mismatched Gem Versions 45min\n' +
+                '12:00PM Lunch\n' +
+                '01:00PM Ruby on Rails: Why We Should Move On 60min\n' +
+                '02:00PM Common Ruby Errors 45min\n' +
+                '02:45PM Pair Programming vs Noise 45min\n' +
+                '03:30PM Programming in the Boondocks of Seattle 30min\n' +
+                '04:00PM Ruby vs. Clojure for Back-End Development 30min\n' +
+                '04:30PM User Interface CSS in Rails Apps 30min\n' +
+                '05:00PM Networking Event'
+
+        new TrackData([new TalkData("Communicating Over Distance", 60),
+                       new TalkData("Rails Magic", 60),
+                       new TalkData("Woah", 30),
+                       new TalkData("Sit Down and Write", 30)],
+                      [new TalkData("Accounting-Driven Development", 45),
+                       new TalkData("Clojure Ate Scala (on my project)", 45),
+                       new TalkData("A World Without HackerNews", 30),
+                       new TalkData("Ruby on Rails Legacy App Maintenance", 60),
+                       new TalkData("Rails for Python Developers", 5)])       |
+                2                                                                          ||
+                'Track 2\n' +
                 '09:00AM Communicating Over Distance 60min\n' +
                 '10:00AM Rails Magic 60min\n' +
                 '11:00AM Woah 30min\n' +
@@ -35,12 +66,6 @@ class TrackDataToStringSerializerSpecTest extends Specification {
                 '04:00PM Rails for Python Developers lightning\n' +
                 '04:05PM Networking Event'
         // TODO: Adjustment for specification error, we assume that the Networking Event doesn't have to start at a full hour
-
-        when:
-        def output = serializer.serialize(input, 1)
-
-        then:
-        expectedOutput == output
     }
 
     def "should throw if input is null"() {
