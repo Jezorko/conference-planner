5b1cd475775751cc6549835229969751f8040b68
Paweł Fiuk <pawelfiu@gmail.com>
2017-10-15 22:04:54 +0200

    Implementation of TrackDataWriter
     - added objenesis dependency

--- src/main/java/io/TrackDataWriter.java
+++ src/main/java/io/TrackDataWriter.java
@@ -6,12 +6,20 @@ import lombok.RequiredArgsConstructor;
 import java.io.PrintStream;
 import java.util.List;
 
+import static io.vavr.Tuple.of;
+import static java.util.stream.IntStream.range;
+
 @RequiredArgsConstructor
 public class TrackDataWriter {
 
     private final TrackDataToStringSerializer serializer;
 
     public void writeTo(PrintStream printStream, List<TrackData> conferenceData) {
-        // TODO: provide implementation
+        if (printStream == null || conferenceData == null) {
+            throw new IllegalArgumentException("Input may not be null");
+        }
+        range(0, conferenceData.size()).mapToObj(index -> of(conferenceData.get(index), index + 1))
+                                       .map(data -> serializer.serialize(data._1(), data._2()))
+                                       .forEach(printStream::println);
     }
 }
