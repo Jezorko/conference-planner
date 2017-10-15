50c4a086e38e5ccb1a4e05f6d03431b7330f0005
Paweł Fiuk <pawelfiu@gmail.com>
2017-10-15 22:49:44 +0200

    Created test for TracksAssigningAlgorithm
    There are only two test cases, one of which
    was taken directly from the specification,
    because the algorithm seems at the moment to be
    too complex to predict its' other requirements.

---
+++ src/main/java/core/ConferenceTracksAssigningAlgorithm.java
@@ -0,0 +1,9 @@
+package core;
+
+import java.util.List;
+
+public class ConferenceTracksAssigningAlgorithm {
+    List<TrackData> assignToTracks(List<TalkData> talks) {
+        return null;
+    }
+}
---
+++ src/test/groovy/core/ConferenceTracksAssigningAlgorithmSpecTest.groovy
@@ -0,0 +1,70 @@
+package core
+
+import spock.lang.Specification
+
+class ConferenceTracksAssigningAlgorithmSpecTest extends Specification {
+
+    static SPECIFICATION_TALKS = [
+            new TalkData("Writing Fast Tests Against Enterprise Rails", 60),
+            new TalkData("Overdoing it in Python", 45),
+            new TalkData("Lua for the Masses", 30),
+            new TalkData("Ruby Errors from Mismatched Gem Versions", 45),
+            new TalkData("Common Ruby Errors", 45),
+            new TalkData("Rails for Python Developers", 5),
+            new TalkData("Communicating Over Distance", 60),
+            new TalkData("Accounting-Driven Development", 45),
+            new TalkData("Woah", 30),
+            new TalkData("Sit Down and Write", 30),
+            new TalkData("Pair Programming vs Noise", 45),
+            new TalkData("Rails Magic", 60),
+            new TalkData("Ruby on Rails: Why We Should Move On", 60),
+            new TalkData("Clojure Ate Scala (on my project)", 45),
+            new TalkData("Programming in the Boondocks of Seattle", 30),
+            new TalkData("Ruby vs. Clojure for Back-End Development", 30),
+            new TalkData("Ruby on Rails Legacy App Maintenance", 60),
+            new TalkData("A World Without HackerNews", 30),
+            new TalkData("User Interface CSS in Rails Apps", 30)
+    ]
+
+    def algorithm = new ConferenceTracksAssigningAlgorithm()
+
+    def "should generate optimal solution"() {
+        given:
+        def morningSessionMaximumTimeInMinutes = 3 * 60
+        def afternoonSessionMaximumTimeInMinutes = 4 * 60
+
+        when:
+        def tracks = algorithm.assignToTracks SPECIFICATION_TALKS
+
+        then: "there are only two tracks"
+        tracks.size() == 2 as int
+
+        and: "each track has valid session times"
+        tracks.forEach {
+            assert SPECIFICATION_TALKS.containsAll(it.morningSessionTalks)
+            assert it.morningSessionTalks.stream().map({ it.lengthInMinutes }).reduce(Integer.&sum) <= morningSessionMaximumTimeInMinutes
+
+            assert SPECIFICATION_TALKS.containsAll(it.afternoonSessionTalks)
+            assert it.afternoonSessionTalks.stream().map({ it.lengthInMinutes }).reduce(Integer.&sum) <= afternoonSessionMaximumTimeInMinutes
+        }
+
+        and: "there are no talks lost in the process"
+        def resultingTalks = []
+        tracks.forEach {
+            resultingTalks.addAll(it.morningSessionTalks)
+            resultingTalks.addAll(it.afternoonSessionTalks)
+        }
+        resultingTalks.containsAll(SPECIFICATION_TALKS)
+    }
+
+    def "should throw if the input is null"() {
+        given:
+        List<TalkData> talks = null
+
+        when:
+        algorithm.assignToTracks talks
+
+        then:
+        thrown IllegalArgumentException
+    }
+}
