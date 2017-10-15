caa3ee3ba16fd434d7ab894a7fe2244f8e6e5dd6
Paweł Fiuk <pawelfiu@gmail.com>
2017-10-15 19:52:36 +0200

    Made TalkData comparable

--- src/main/java/core/TalkData.java
+++ src/main/java/core/TalkData.java
@@ -2,8 +2,15 @@ package core;
 
 import lombok.Value;
 
+import static java.lang.Integer.valueOf;
+
 @Value
-public class TalkData {
+public class TalkData implements Comparable<TalkData> {
     private final String name;
     private final int lengthInMinutes;
+
+    @Override
+    public int compareTo(TalkData other) {
+        return valueOf(other.lengthInMinutes).compareTo(this.lengthInMinutes);
+    }
 }
